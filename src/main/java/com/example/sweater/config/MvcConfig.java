0a1279214421061dd7887e35e8591e16f5c76f9c
Ivan Viktorv <Meet      Hangouts  2  5 320 mbuyakov invited you to mbuyakov/ETP.ReportServer   mbuyakov noreply@github.com 11:58 (1  ) :   GitHub home @mbuyakov plus @ivor108 @mbuyakov has invited you to collaborate on the mbuyakov/ETP.ReportServer repository You can accept or decline this invitation. You can also visit @mbuyakov to learn a bit more about them.  This invitation will expire in 7 days.  View invitation Note: This invitation was intended for ivor108@gmail.com. If you were not expecting this invitation, you can ignore this email. If @mbuyakov is sending you too many emails, you can block them or report abuse.  Getting a 404 error? Make sure youre signed in as ivor108.  Button not working? Copy and paste this link into your browser: https://github.com/mbuyakov/ETP.ReportServer/invitations>
2021-09-07 13:31:07 +0300

    4 lesson

--- src/main/java/com/example/sweater/config/MvcConfig.java
+++ src/main/java/com/example/sweater/config/MvcConfig.java
@@ -7,7 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login.ftlh");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/main").setViewName("main.ftlh");
+        registry.addViewController("/registration").setViewName("registration.ftlh");
     }
 
 }
