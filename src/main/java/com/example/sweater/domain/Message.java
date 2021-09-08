c6d6c5f159f6f0094d4118c7068899cd9c64c803
Ivan Viktorv <Meet      Hangouts  2  5 320 mbuyakov invited you to mbuyakov/ETP.ReportServer   mbuyakov noreply@github.com 11:58 (1  ) :   GitHub home @mbuyakov plus @ivor108 @mbuyakov has invited you to collaborate on the mbuyakov/ETP.ReportServer repository You can accept or decline this invitation. You can also visit @mbuyakov to learn a bit more about them.  This invitation will expire in 7 days.  View invitation Note: This invitation was intended for ivor108@gmail.com. If you were not expecting this invitation, you can ignore this email. If @mbuyakov is sending you too many emails, you can block them or report abuse.  Getting a 404 error? Make sure youre signed in as ivor108.  Button not working? Copy and paste this link into your browser: https://github.com/mbuyakov/ETP.ReportServer/invitations>
2021-09-08 17:53:24 +0300

    11 lesson

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,6 +1,10 @@
 package com.example.sweater.domain;
 
+import com.sun.istack.NotNull;
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Message {
@@ -8,7 +12,10 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message="Please fill th message")
+    @Length(max = 2048, message = "Message too long")
     private String text;
+    @Length(max = 255, message = "Tag too long")
     private String tag;
 
     @ManyToOne(fetch = FetchType.EAGER)
