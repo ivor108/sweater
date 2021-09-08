cf5558bcaa12f900471072b3122ae3b8a0f31dc7
Ivan Viktorv <Meet      Hangouts  2  5 320 mbuyakov invited you to mbuyakov/ETP.ReportServer   mbuyakov noreply@github.com 11:58 (1  ) :   GitHub home @mbuyakov plus @ivor108 @mbuyakov has invited you to collaborate on the mbuyakov/ETP.ReportServer repository You can accept or decline this invitation. You can also visit @mbuyakov to learn a bit more about them.  This invitation will expire in 7 days.  View invitation Note: This invitation was intended for ivor108@gmail.com. If you were not expecting this invitation, you can ignore this email. If @mbuyakov is sending you too many emails, you can block them or report abuse.  Getting a 404 error? Make sure youre signed in as ivor108.  Button not working? Copy and paste this link into your browser: https://github.com/mbuyakov/ETP.ReportServer/invitations>
2021-09-08 13:44:26 +0300

    9 lesson

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -10,11 +10,15 @@ import java.util.Set;
 @Entity
 @Table(name = "usr")
 public class User implements UserDetails {
-    @Id@GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String username;
     private String password;
     private boolean active;
+
+    private String email;
+    private String activationCode;
 
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
@@ -89,4 +93,21 @@ public class User implements UserDetails {
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
+    }
 }
+
